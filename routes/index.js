db5343153677db41d041dcd69371769905c6b3c6
Calvin Gomez <calvin.gomez1@gmail.com>
2016-05-24 22:16:45 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/menu', function(req, res, next) {
+  res.render('menu');
+});
+
+router.get('/maptest', function(req, res, next) {
+  res.render('maptest');
+});
+
+module.exports = router;
