e97e4038ff91bc30761774d919dc24a9aeaebf89
Diana Ho <dianahox3@gmail.com>
2016-05-30 14:08:01 -0700

    Added city markers

--- public/js/maptest.js
+++ public/js/maptest.js
@@ -6,11 +6,31 @@ var mymap = L.map('mapid', { zoomControl: false }).setView([32.7157, -117.1611],
     accessToken: 'pk.eyJ1IjoiYXByYXRobmEiLCJhIjoiY2lva3R0dTBkMDAzYXU2bTA3bXVjdXpvZSJ9.vnChROM_wM-N55VCqPJRTg'
 }).addTo(mymap);
 
-		var carslbadMarker = L.marker([33.1581, -117.3506]).on('click', altOnClick).addTo(mymap);
-		carslbadMarker.bindPopup("Carlsbad").openPopup();
+		var carslbadMarker = L.marker([33.1581, -117.3506]).on('click', altOnClick).bindLabel('Carlsbad', { noHide: true }).addTo(mymap);
 
-		var laJollaMarker = L.marker([32.8328, -117.2713]).on('click', altOnClick).addTo(mymap);		
-		laJollaMarker.bindPopup("La Jolla").openPopup();
+		var laJollaMarker = L.marker([32.8328, -117.2713]).on('click', altOnClick).bindLabel('La Jolla', { noHide: true }).addTo(mymap);
+
+        var ramonaMarker = L.marker([33.0414, -116.8793]).on('click', altOnClick).bindLabel('Ramona', { noHide: true }).addTo(mymap);
+
+        var sanDieguitoMarker = L.marker([32.9990, -117.2338]).on('click', altOnClick).bindLabel('San Dieguito', { noHide: true }).addTo(mymap);
+
+        var sanMarcosMarker = L.marker([33.1434, -117.1661]).on('click', altOnClick).bindLabel('San Marcos', { noHide: true }).addTo(mymap);
+
+        var santeeMarker = L.marker([32.8384, -116.9739]).on('click', altOnClick).bindLabel('Santee', { noHide: true }).addTo(mymap);
+
+        var southBayMarker = L.marker([32.5549, -117.044306]).on('click', altOnClick).bindLabel('South Bay', { noHide: true }).addTo(mymap);
+
+        var southEasternSanDiegoMarker = L.marker([32.7004, -117.1432]).on('click', altOnClick).bindLabel('South Eastern San Diego', { noHide: true }).addTo(mymap);
+
+        var springValleyMarker = L.marker([32.444, -116.5853]).on('click', altOnClick).bindLabel('Spring Valley', { noHide: true }).addTo(mymap);
+
+        var sweetwaterMarker = L.marker([32.4149, -116.5914]).on('click', altOnClick).bindLabel('Sweetwater', { noHide: true }).addTo(mymap);
+
+        var universityCityMarker = L.marker([33.87, -117.21]).on('click', altOnClick).bindLabel('University City', { noHide: true }).addTo(mymap);
+
+        var valleyCenterMarker = L.marker([33.1426, -117.051]).on('click', altOnClick).bindLabel('Valley Center', { noHide: true }).addTo(mymap);
+
+        var vistaMarker = L.marker([33.1137, -117.1428]).on('click', altOnClick).bindLabel('Vista', { noHide: true }).addTo(mymap);
 
         new L.Control.Zoom({ position: 'bottomleft' }).addTo(mymap);
 
