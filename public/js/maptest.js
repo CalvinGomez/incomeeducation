27ae1d3b23efeed2eeaecac0b61b8a5a2ce3e6e7
James Kong <jamesbkong@gmail.com>
2016-05-30 14:06:52 -0700

    Recentered default map view

--- public/js/maptest.js
+++ public/js/maptest.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid', { zoomControl: false }).setView([32.7157, -117.1611], 10);
+var mymap = L.map('mapid', { zoomControl: false }).setView([32.9000, -116.8000], 10);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
     maxZoom: 18,
